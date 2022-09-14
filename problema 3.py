0688d4aa1fdb185c7608c8a224a88771156cb758
Alexandra-Sontu <71020688+Alexandra-Sontu@users.noreply.github.com>
2022-09-14 11:07:22 +0200

    Add files via upload

---
+++ problema 3.py
@@ -0,0 +1,9 @@
+a = int(input("Introduce a: "))
+b = int(input("Introduce b: "))
+c = int(input("Introduce c: "))
+d = int(input("Introduce d: "))
+
+def fractionoperation():
+    print(f'Suma lor este {((a*d) + (c*b)) / (b*d)}, iar produsul lor este {(a*c) / (b*d)}')
+
+fractionoperation()
